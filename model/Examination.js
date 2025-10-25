250b91ac5d0b7e288042128768c15f39e48a3397
akhil5564 <74394421+akhil5564@users.noreply.github.com>
2025-10-26 00:42:59 +0530

    Add files via upload

---
+++ model/Examination.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const questionSchema = new Schema({
+  question: { type: String, required: true },
+  questionType: { type: String, required: true },
+  mark: { type: Number, required: true },
+  options: [String], // Optional, for MCQ
+  answer: String     // Optional, for subjective
+}, { _id: false });
+
+const examinationSchema = new Schema({
+  school: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  subject: { type: String, required: true },
+  class: { type: String, required: true },
+  book: { type: String, required: true },
+  chapters: [{ type: String, required: true }],
+  examinationType: { type: String, required: true },
+  totalMark: { type: Number, required: true },
+  duration: { type: Number, required: true },
+  schoolName: { type: String, required: true },
+  questions: [questionSchema]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Examination', examinationSchema);
